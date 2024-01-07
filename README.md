c8b418b741869c04015e215f3971697097b6ba1f
Dennis Gilli <dennis.gilli@acribis.ch>
2024-01-07 10:22:59 +0100

    add easy svg line drawer

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # About Dioxus Draw
 
 Simple Proof-of-Concept drawing app in rust using dioxus to render natively on desktop. I hope this will also entertain my gremlin for a while...
+
+Run the thing:
+```sh
+rm -rf dist && dx serve --platform desktop
+```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
 #![allow(non_snake_case)]
-// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
 use dioxus::prelude::*;
+use log::{info, LevelFilter};
 
 fn main() {
-    // launch the dioxus app in a webview
+    dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
     dioxus_desktop::launch(App);
 }
 
-// define a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
+    let start_coords = use_state(&cx, || (0f32, 0f32));
+    let end_coords = use_state(&cx, || (0f32, 0f32));
+
     cx.render(rsx! {
         div {
-            "Hello, world!"
+            style: "position: absolute; top: 0; left: 0",
+            width: "100%",
+            height: "100%",
+            onmousemove: move |event| {
+                end_coords.set((event.client_x as f32, event.client_y as f32));
+            },
+            onmousedown: move |event| {
+                let mouse_x = event.client_x;
+                let mouse_y = event.client_y;
+                log::info!("Mouse position: x = {mouse_x}, y = {mouse_y}");
+                start_coords.set((mouse_x as f32, mouse_y as f32));
+            },
+            svg {
+                width: "100%",
+                height: "100%",
+                line {
+                    x1: "{start_coords.0}",
+                    y1: "{start_coords.1}",
+                    x2: "{end_coords.0}",
+                    y2: "{end_coords.1}",
+                    stroke: "black",
+                    stroke_width: "2"
+                }
+            }
         }
     })
 }
